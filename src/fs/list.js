0dfb0c739e6e61d545be45b97641aa89b7485a6a
Alena Deviatova <aedeviatova@yandex.ru>
2023-06-13 18:06:38 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import {promises as fs} from "fs";
+import {dirname, join} from "path";
+import {fileURLToPath} from "url";
+import {isFileExist} from "../utils/fs.js";
+
 const list = async () => {
-    // Write your code here 
+
+    const curDir = dirname(fileURLToPath(import.meta.url))
+    const filesPath = join(curDir, 'files')
+    const error = 'FS operation failed: no such file'
+
+    if (!await isFileExist(filesPath)) {
+        throw Error(error)
+    }
+
+    const files = await fs.readdir(filesPath, {recursive: true})
+    console.log(files)
+
 };
 
 await list();
