913b30cd81350a6bbd4927c1cfdff6be711b1eb3
Alena Deviatova <aedeviatova@yandex.ru>
2023-06-13 17:53:00 +0300

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import {dirname, join} from "path";
+import {fileURLToPath} from "url";
+import {isFileExist} from "../utils/fs.js";
+import {promises as fs} from "fs";
+
 const remove = async () => {
-    // Write your code here 
+    const curDir = dirname(fileURLToPath(import.meta.url))
+    const file  = join(curDir, 'files', 'fileToRemove.txt')
+    const error = 'FS operation failed: no such file'
+
+    if(!await isFileExist(file)) {
+        throw Error(error)
+    } else {
+        await fs.rm(file)
+    }
 };
 
 await remove();
