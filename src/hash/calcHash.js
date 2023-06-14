ca343f8aa213b9294094c118756b372e52279fc8
Alena Deviatova <aedeviatova@yandex.ru>
2023-06-14 14:20:20 +0300

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import {promises as fs} from "fs";
+import {dirname, join} from "path";
+import {fileURLToPath} from "url";
+import crypto from "crypto";
+import {isFileExist} from "../utils/fs.js";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const curDir = dirname(fileURLToPath(import.meta.url))
+    const file = join(curDir, 'files', 'fileToCalculateHashFor.txt')
+    const error = 'No such file'
+
+    if (!await isFileExist(file)) {
+        throw Error(error)
+    }
+
+    const fileContent = await (fs.readFile(file))
+    const hash = crypto.createHash('sha256').update(fileContent).digest('hex');
+
+    console.log(hash);
 };
 
 await calculateHash();
