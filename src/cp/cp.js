28740395c4da7776e580bc0a4e85718897d23ef4
Alena Deviatova <aedeviatova@yandex.ru>
2023-06-18 11:58:55 +0300

    implement child process function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import {fork} from "child_process";
+import {dirname, join} from "path";
+import {fileURLToPath} from "url";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const filePath = join(dirname(fileURLToPath(import.meta.url)), 'files', 'script.js')
+    const child = fork(filePath, args, { stdio: 'pipe' });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+try {
+    await spawnChildProcess(['arg1', 'arg2', 'arg3']);
+} catch (error) {
+    console.error('There was an error:', error);
+}
